87df4ea0a1e98fe5eaf209135d8a591764505be2
Ju <tumsebiene@web.de>
2018-06-20 13:08:08 +0200

    added database dummy

---
+++ dbmanager.py
@@ -0,0 +1,15 @@
+import sqlite3 as sql
+
+
+class db(sqlite3.database):
+    
+    def __init__(this):
+    
+    def getM(this):
+        return 10
+    
+    def initT(this,ktof,ktot,msum):
+        return 200
+    
+    def setT(this,rid):
+        return 200

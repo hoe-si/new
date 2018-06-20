99b03e1f3e54c82503d8966fd446c89fbf3264ec
Ju <tumsebiene@web.de>
2018-06-20 09:13:24 +0200

    makes hello side

---
+++ server.py
@@ -0,0 +1,13 @@
+import http.server
+import os
+
+os.chdir("website")
+
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+run()
+

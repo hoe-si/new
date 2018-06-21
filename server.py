386b07a59e58373981ec0ee86da907829ff1265a
krichenbauer <schule@krichenbauer.de>
2018-06-21 13:29:32 +0200

    post request

--- server.py
+++ server.py
@@ -4,4 +4,14 @@ from bottle import route, run, static_file, template, request
 def page_init():
   return template('./website/init.html', kto=request.query.kto or "fehlerhaft")
 
+
+@route('/pin.html', method='POST')
+def page_pin():
+  params = {
+    "vonkto":request.forms.get("vonkto"),
+    "ankto":request.forms.get("ankto"),
+    "betrag":request.forms.get("betrag")}
+  
+  return template('./website/pin.html', **params)
+
 run(host='localhost', port=8000, debug=True)

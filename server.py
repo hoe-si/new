d2550c1e18ccaca240581c409fe34f149dc1248c
Ju <tumsebiene@web.de>
2018-06-21 10:19:37 +0200

    tried to implement post requests

--- server.py
+++ server.py
@@ -3,10 +3,11 @@ import os
 
 
 repdict={"name":"Julius","knf":"!search","stand":"1"}
-sites={"/index.html":("name","knf","stand"),
-"/init.html":("knf",),
-"/css/style.css":(),
-"pin.html":("knf",)}
+sites={"/index.html":{"knf":"!search"},
+"/init.html":{"knf":"!search"},
+"/css/style.css":{},
+"pin.html":{"knf":"!data","knt":"!data","mnt":"!data"},
+"return.html":{"knf":"!data","knt":"!data","mnt":"!data"}}
 
 
 
@@ -16,72 +17,116 @@ sites={"/index.html":("name","knf","stand"),
 class MyHTTPRequestHandler(BaseHTTPRequestHandler):
     
     def do_GET(this):
-        a=resource(this.path)
-        a.replace()
-        if a.getState()==200:
-            this.send_response(200)
-            this.end_headers()
-            this.wfile.write(a.getEnd())
-        else:
-            this.send_error(a.getState())
+        this.anyway(this.path,data("dummy",""))
     
     def do_POST(this):
-        a=resource(this.path)
-        
+        try:
+            LEN= int(self.headers.get("content-length"))
+            pack = self.rfile.read(LEN)
+        except:
+            print(">> converter: content-length missing or wrong")
+            this.send_error(400)
+            return None
+        this.anyway(this.path,{"!data":pack})
+		
+    def anyway(this,uri,data):
+	    a=resource(uri,data)
+	    print("1. state: "+str(a.getState()))
+	    a.replace()
+	    print("2. state: "+str(a.getState()))
+	    if a.getState()==200:
+	        this.send_response(200)
+	        this.end_headers()
+	        this.wfile.write(a.getEnd())
+	    else:
+	        this.send_error(a.getState())
 
+
+class data(dict):
+    rtype=""
+    
+    def __init__(this,name,raw):
+        super()
+        print("created data object")
+        this.rtype=getStringOf(name)
+        this.update(decparamform(raw))
+        
+        
+    def getBin(this,key):
+        try:
+            return(this.get(getBinOf(key)))
+        except KeyError:
+            return 400
+            
+    def getString(this,key):
+        try:
+            return getStringOf(this[getBinOf(key)])
+        except UnicodeDecodeError:
+            return 400
+        except KeyError:
+            return 400
+    
+    def getValue(this,key):
+        if key in this.keys():
+            return this[key]
+        else:
+            return 400
+            
 
 
 
 class resource:
-    replaces=()
+    # class for any data of which the path is saved in "sites"
+    # it is able to open the file, replace everything there has to be replaced in the site
+    # 
+    replaces={}
     code=b""
     endcode= b""
     uri=""
-    search={}
-    data={}
+    givens={}
     state=200
     
-    def __init__(this,uri):
+    def __init__(this,uri,indata):
         
         temp= uri.split("?")
         this.uri=temp[0]
         if len(temp)>=2:
-            this.search=decparamform(temp[1])
+            params= temp[1]
+            newdata=data("search",params)
+            this.givens.update({"search":newdata})
         if this.uri.endswith("/"):
             this.uri += "index.html"
+        this.givens=indata
         
         try:
-            code= open("website"+this.uri,"rb").read()
-        except:
+            this.replaces=sites[this.uri]
+        except KeyError:
             this.state= 404
             return None
+        code= open("website"+this.uri,"rb").read()
         this.endcode= code
-        this.replaces = getSreps(this.uri)
-        this.status= 200
+        this.state= 200
     
     def replace(this):
         for i in this.replaces:
             if this.state==200:
                 this.state= this.repOne(i)
             else:
+                print(i)
                 return this.state
     
     def repOne(this,rep):
-        repn=repdict[rep]
+        repn=repdict[rep]   #rep was given by the sites dict
         rep=getBinOf(rep)
-        rep= b"[$"+rep+ b"$]"
-        if repn == "!search":
-            if "knf" in this.search.keys():
-                repn= this.search["knf"]
-            else:
-                return 400
-        
-        repn=getBinOf(repn)
-        this.endcode= this.endcode.replace(rep,repn)
-        return 200
-    
-    def setData(this,data):
-        this.data= decparamform(data)
+        rep= b"[$"+rep+ b"$]"   #the full string which has to be replaced
+        by=this.givens.get(repn,400)
+        if type(by)==data:
+            by=by.getBin(repn)
+        if type(by)==data:
+            this.endcode= this.endcode.replace(rep,repn)
+            return 200
+        else:
+            return by   #is html error (int) if anything went wrong
     
     def getState(this):
         return this.state
@@ -89,8 +134,6 @@ class resource:
     def getEnd(this):
         return this.endcode
 
-def getSreps(uri):
-    return sites[uri]
 
 
 def run(server_class=HTTPServer, handler_class=MyHTTPRequestHandler):
@@ -99,19 +142,27 @@ def run(server_class=HTTPServer, handler_class=MyHTTPRequestHandler):
     httpd.serve_forever()
 
 def decparamform(parastring):
-	# the parameterstring will be called search here
-	params={}
-	spsearch=parastring.split("&")    #different search arguments
-	for i in spsearch:    #update key:parameter dictionnary
-		a=i.split("=")
-		if len(a)==2:
-			params.update({a[0]:a[1]})
-	
-	return params
+    # sets keys to bytestring format for making it easyer to work with given resource
+    # the parameterstring will be called search here
+    params={}
+    spsearch=parastring.split("&")    #different search arguments
+    for i in spsearch:    #update key:parameter dictionnary
+	    a=i.split("=")
+	    if len(a)==2:
+	        params.update({getBinOf(a[0]):a[1]})
+	    else:
+	        params.update({b"default":a[0]})
+    
+    return params
 
 def getBinOf(inp):
     if type(inp)==str:
         inp=inp.encode()
+    return inp
+
+def getStringOf(inp):
+    if type(inp)==bytes:
+        inp=inp.decode()
     return inp
 
 

ef400571d962ad686bbdadfe453121a961891865
network4u <tumsebiene@web.de>
2018-07-03 11:44:58 +0200

    an mysql Syntax angepasst

--- dbmanager.py
+++ dbmanager.py
@@ -21,11 +21,13 @@ class DB():
     def getKontostand(this,kto):
         money = 0
         sql="select betrag from transaktion where ankto='"+str(kto)+"' AND erledigt=1;"
-        add = this.db.execute(sql).fetchall()
+        this.db.execute(sql)
+        add = this.db.fetchall()
         for i in add:
             money+=int(i[0])
         sql="select betrag from transaktion where vonkto='"+str(kto)+"' AND erledigt=1;"
-        sub = this.db.execute(sql).fetchall()
+        this.db.execute(sql).
+        sub = this.db.fetchall()
         for i in sub:
             money-=int(i[0])
         return money
@@ -34,7 +36,8 @@ class DB():
     def initTransaktion(this,ktof,ktot,msum):
         #Generate the random tid
         tid=random.randint(1000000,9999999)
-        while(len(this.db.execute("select * from transaktion where tid='"+str(tid)+"';").fetchmany(100))>=1):
+        this.db.execute("select * from transaktion where tid='"+str(tid)+"';")
+        while(len(this.db.fetchmany(100))>=1):
             tid=random.randint(1000,9999)
         #genereate the timestamp
         apfel = time()
@@ -47,7 +50,8 @@ class DB():
         
     #confirm the transaction
     def setErledigt(this,tid,vonkto,ankto,betrag):
-        apfel = this.db.execute("select * from transaktion where tid='"+str(tid)+"' and vonkto='"+str(vonkto)+"' and ankto='"+str(ankto)+"' and betrag='"+str(betrag)+"';").fetchmany(2)
+        this.db.execute("select * from transaktion where tid='"+str(tid)+"' and vonkto='"+str(vonkto)+"' and ankto='"+str(ankto)+"' and betrag='"+str(betrag)+"';")
+        apfel = this.db.fetchmany(2)
         if(len(apfel)>= 1):
             this.db.execute("update transaktion set erledigt = 1 where tid='"+str(tid)+"';")
             this.dbfile.commit()
@@ -58,9 +62,11 @@ class DB():
     def checkPin(this, kto, pin):
 
         checkLogfileSql="select * from logfile where erledigt=0 and zeit > " + str(time()-5*60) + " and  kto='"+str(kto) + "';"
-        wrongKeyTries=this.db.execute(checkLogfileSql).fetchmany(101)
+        this.db.execute(checkLogfileSql)
+        wrongKeyTries=this.db.fetchmany(101)
         
-        f = this.db.execute("select pin from konto where kto='"+str(kto)+"' and pin='"+str(pin) + "';").fetchmany(2)
+        this.db.execute("select pin from konto where kto='"+str(kto)+"' and pin='"+str(pin) + "';")
+        f = this.db.fetchmany(2)
         success = len(f)==1
         timestamp = time()
         logfile_sql="insert into logfile(zeit,kto,erledigt) values ('" + str(timestamp) + "','" + str(kto) + "','" + str(int(success)) + "');"
@@ -122,6 +128,7 @@ class DBmysql(DB):
     
     def __init__(this):
         #set a database
+        super()
         this.dbfile = pymysql.connect(host='localhost',
                                       user=conf["user"],
                                       password=conf["password"],

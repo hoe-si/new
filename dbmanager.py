3f639fae7f9b4b6d84e65d2946a837754745f620
network4u <tumsebiene@web.de>
2018-07-03 11:26:13 +0200

    zwei DB klassen zu einer Superklasse vereint

--- dbmanager.py
+++ dbmanager.py
@@ -5,14 +5,17 @@ from time import time
 
 from dbconf import a as conf
 
-class DBsqlite():
+
+
+
+
+
+
+class DB():
     
     def __init__(this):
         #set a database
-        this.dbpath = "database.db"
-        this.dbfile = sqlite3.connect(this.dbpath)
-        this.db = this.dbfile.cursor()
-        print("apfel")
+        return None
         
     #get the money
     def getKontostand(this,kto):
@@ -63,14 +66,59 @@ class DBsqlite():
         logfile_sql="insert into logfile(zeit,kto,erledigt) values ('" + str(timestamp) + "','" + str(kto) + "','" + str(int(success)) + "');"
         this.db.execute(logfile_sql)
         return success and len(wrongKeyTries) <= 100
+
+
+
+
+
+
+
+
+
+
+
+class DBsqlite(DB):
+    
+    def __init__(this):
+        #set a database
+        super()
+        this.dbpath = "database.db"
+        this.dbfile = sqlite3.connect(this.dbpath)
+        this.db = this.dbfile.cursor()
+        print("apfel")
         
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #---------------------------------------------------------#
 #---------------------------------------------------------#
 #---------------------------------------------------------#
 #               MySql database:                           #
 #---------------------------------------------------------#
 
-class DBmysql():
+
+
+
+
+
+
+
+
+class DBmysql(DB):
     
     def __init__(this):
         #set a database
@@ -82,55 +130,5 @@ class DBmysql():
                                       cursorclass=pymysql.cursors.DictCursor)
         this.db = this.dbfile.cursor()
         print("apfel")
-        
-    #get the money
-    def getKontostand(this,kto):
-        money = 0
-        sql="select betrag from transaktion where ankto='"+str(kto)+"' AND erledigt=1;"
-        add = this.db.execute(sql).fetchall()
-        for i in add:
-            money+=int(i[0])
-        sql="select betrag from transaktion where vonkto='"+str(kto)+"' AND erledigt=1;"
-        sub = this.db.execute(sql).fetchall()
-        for i in sub:
-            money-=int(i[0])
-        return money
-        
-    #initialise transaction
-    def initTransaktion(this,ktof,ktot,msum):
-        #Generate the random tid
-        tid=random.randint(1000000,9999999)
-        while(len(this.db.execute("select * from transaktion where tid='"+str(tid)+"';").fetchmany(100))>=1):
-            tid=random.randint(1000,9999)
-        #genereate the timestamp
-        apfel = time()
-        #prouce the sql
-        vls = "("+str(tid)+","+str(ktof)+","+str(ktot)+","+str(msum)+",0,'"+str(apfel)+"')"
-        #execute it!
-        this.db.execute("insert into transaktion (tid,vonkto,ankto,betrag,erledigt,zeit) values "+vls+";")
-        this.dbfile.commit()
-        return tid
-        
-    #confirm the transaction
-    def setErledigt(this,tid,vonkto,ankto,betrag):
-        apfel = this.db.execute("select * from transaktion where tid='"+str(tid)+"' and vonkto='"+str(vonkto)+"' and ankto='"+str(ankto)+"' and betrag='"+str(betrag)+"';").fetchmany(2)
-        if(len(apfel)>= 1):
-            this.db.execute("update transaktion set erledigt = 1 where tid='"+str(tid)+"';")
-            this.dbfile.commit()
-            return True
-        return False
-        
-    #check for the pin
-    def checkPin(this, kto, pin):
-
-        checkLogfileSql="select * from logfile where erledigt=0 and zeit > " + str(time()-5*60) + " and  kto='"+str(kto) + "';"
-        wrongKeyTries=this.db.execute(checkLogfileSql).fetchmany(101)
-        
-        f = this.db.execute("select pin from konto where kto='"+str(kto)+"' and pin='"+str(pin) + "';").fetchmany(2)
-        success = len(f)==1
-        timestamp = time()
-        logfile_sql="insert into logfile(zeit,kto,erledigt) values ('" + str(timestamp) + "','" + str(kto) + "','" + str(int(success)) + "');"
-        this.db.execute(logfile_sql)
-        return success and len(wrongKeyTries) <= 100
         
 
